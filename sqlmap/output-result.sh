f2c7e77cee1bf2d50fcd11edd360481358ddef16
w2seraph <listen.host@gmail.com>
2022-11-20 02:42:41 +0100

    Update output-result.sh

--- sqlmap/output-result.sh
+++ sqlmap/output-result.sh
@@ -1,3 +1,3 @@
-cat /opt/openssl/root/.local/share/sqlmap/output/*.csv > vuln.out
-python3 /opt/openssl/root/same-domain-vuln.py
-python3 /opt/openssl/root/same-domain-linkz.py
+cat ~/.local/share/sqlmap/output/*.csv > vuln.out
+python3 ~/same-domain-vuln.py
+python3 ~/same-domain-linkz.py
